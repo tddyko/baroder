37bd865fefcb8ffb11c27d3339cdf8902c7655f2
TomKo <tom.koh@cobosys.co.kr>
2021-12-22 22:42:16 +0900

    바로더 기본세팅중

--- app/src/main/java/co/kr/cobosys/baroder/app/Application.kt
+++ app/src/main/java/co/kr/cobosys/baroder/app/Application.kt
@@ -1,8 +1,34 @@
 package co.kr.cobosys.baroder.app
 
+import android.os.StrictMode
 import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class Application: MultiDexApplication() {
+class Application : MultiDexApplication() {
+
+    override fun onCreate() {
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork() // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    //.penaltyDeath() //TODO
+                    .build()
+            )
+        }
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
 }
--- app/src/main/java/co/kr/cobosys/baroder/main/MainActivity.kt
+++ app/src/main/java/co/kr/cobosys/baroder/main/MainActivity.kt
@@ -2,13 +2,60 @@ package co.kr.cobosys.baroder.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import co.kr.cobosys.baroder.app.R
 import co.kr.cobosys.baroder.app.databinding.ActivityMainBinding
+import co.kr.cobosys.baroder.extension.viewInflateBinding
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private val binding by viewInflateBinding(ActivityMainBinding::inflate)
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTheme(R.style.Theme_Baroder)
     }
+
+
+    override fun onSupportNavigateUp() = navController.navigateUp()
+
+    override fun onBackPressed() {
+        if (isTaskRoot
+            && supportFragmentManager.primaryNavigationFragment?.childFragmentManager?.backStackEntryCount == 0
+            && supportFragmentManager.backStackEntryCount == 0
+        ) {
+            finishAfterTransition()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onDestroy() {
+        if (isTaskRoot && isFinishing) {
+            finishAfterTransition()
+        }
+        super.onDestroy()
+    }
+
+    private fun setUpNav() {
+
+
+
+        val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.home_fragment,
+                R.id.store_fragment,
+                R.id.coupon_fragment,
+                R.id.point_fragment,
+                R.id.shop_fragment
+            )
+        )
+
+    }
+
 }
