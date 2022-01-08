c56cc6aa2a65e8c210b001fb84278d5dff056fdd
tom.koh <tom.koh@cobosys.co.kr>
2022-01-09 02:41:27 +0900

    뷰 수정중[#2]

--- domain/src/main/java/co/kr/cobosys/domain/usecase/GetFavoriteStoreListUseCase.kt
+++ domain/src/main/java/co/kr/cobosys/domain/usecase/GetFavoriteStoreListUseCase.kt
@@ -1,6 +1,5 @@
 package co.kr.cobosys.domain.usecase
 
-import androidx.paging.PagingData
 import co.kr.cobosys.domain.base.usecase.GeneralUseCase
 import co.kr.cobosys.domain.model.FavoriteStore
 import co.kr.cobosys.domain.repo.BaroderListRepository
