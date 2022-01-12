cc21624e93c8149e503b2c821e562c0d9fe69538
TomKo <tom.koh@cobosys.co.kr>
2022-01-12 11:49:53 +0900

    뷰 수정중[#3]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,14 +89,6 @@ dependencies {
 
     //dependency injection
     implementation(Depends.Libraries.hilt_android)
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
     kapt(Depends.Libraries.hilt_android_compiler)
     kapt(Depends.Libraries.hilt_compiler)
     implementation(Depends.Libraries.java_inject)
