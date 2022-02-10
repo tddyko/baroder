0e8b36144c75ebe2daaee175f46bad9bdf71bf0f
tom.koh <tom.koh@cobosys.co.kr>
2022-02-10 15:07:13 +0900

    공용 앱바 수정

--- app/src/main/java/co/kr/cobosys/baroder/main/MainActivity.kt
+++ app/src/main/java/co/kr/cobosys/baroder/main/MainActivity.kt
@@ -31,7 +31,6 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Theme_Baroder)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         setNav()
     }
 
