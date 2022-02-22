6dc2b16522b5cf87ae833d7c7805d598aad1fc53
TomKo <tom.koh@cobosys.co.kr>
2022-02-22 10:39:15 +0900

    Data Layer Domain Layer 연결 작업중[#2]

--- app/src/main/java/co/kr/cobosys/baroder/auth/signin/SignInViewModel.kt
+++ app/src/main/java/co/kr/cobosys/baroder/auth/signin/SignInViewModel.kt
@@ -1,9 +1,24 @@
 package co.kr.cobosys.baroder.auth.signin
 
+import androidx.lifecycle.viewModelScope
 import co.kr.cobosys.baroder.base.viewmodels.BaseViewModel
+import co.kr.cobosys.domain.models.AccessToken
+import co.kr.cobosys.domain.usecases.auth.GetAccessTokenParams
+import co.kr.cobosys.domain.usecases.auth.GetAccessTokenUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SignInViewModel @Inject constructor(): BaseViewModel() {
+class SignInViewModel @Inject constructor(
+    private val getAccessTokenUseCase: GetAccessTokenUseCase
+): BaseViewModel() {
+    private val _accessToken = MutableStateFlow("access_token")
+    val accessToken = _accessToken.asStateFlow()
+
+    suspend fun getAccessToken(id: String, pwd: String) {
+        getAccessTokenUseCase(GetAccessTokenParams(id = id, pwd = pwd))
+    }
 }
--- data/src/main/java/co/kr/cobosys/data/di/module/RepoModule.kt
+++ data/src/main/java/co/kr/cobosys/data/di/module/RepoModule.kt
@@ -1,21 +1,20 @@
 package co.kr.cobosys.data.di.module
 
 import co.kr.cobosys.data.datasources.auth.AccessTokenDataSource
-import co.kr.cobosys.data.datasources.auth.AccessTokenDataSourceImpl
 import co.kr.cobosys.data.repos.auth.AccessTokenRepoImpl
 import co.kr.cobosys.domain.repos.auth.AccessTokenRepo
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ViewModelComponent::class)
 class RepoModule {
 
     @Provides
-    @ViewModelScoped
+    @Singleton
     fun accessToken(
         accessToken: AccessTokenDataSource
     ): AccessTokenRepo = AccessTokenRepoImpl(accessToken)
