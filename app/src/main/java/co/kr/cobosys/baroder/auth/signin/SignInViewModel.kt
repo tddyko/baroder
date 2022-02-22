5e21be61febdf1bcb54015a197301291e7194f27
TomKo <tom.koh@cobosys.co.kr>
2022-02-22 10:39:38 +0900

    Data Layer Domain Layer 연결 작업중[#2]

--- app/src/main/java/co/kr/cobosys/baroder/auth/signin/SignInViewModel.kt
+++ app/src/main/java/co/kr/cobosys/baroder/auth/signin/SignInViewModel.kt
@@ -1,14 +1,11 @@
 package co.kr.cobosys.baroder.auth.signin
 
-import androidx.lifecycle.viewModelScope
 import co.kr.cobosys.baroder.base.viewmodels.BaseViewModel
-import co.kr.cobosys.domain.models.AccessToken
 import co.kr.cobosys.domain.usecases.auth.GetAccessTokenParams
 import co.kr.cobosys.domain.usecases.auth.GetAccessTokenUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
